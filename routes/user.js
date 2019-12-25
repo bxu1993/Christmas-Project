b281f4d3dfdf165a19445415c9905a2312dd4f94
bxu1993 <xubao2010@gmail.com>
2019-12-25 09:49:50 -0500

    3

--- routes/user.js
+++ routes/user.js
@@ -67,58 +67,98 @@ router.post('/user_create', (req,res) => {
         console.log("Insert a new user with id: ", results.insertId);
         res.end();
     })
-})
+});
 
 
-router.put('/users', (req, res) => {
- 
-  // Get the input from post-man
-  let users_info = req.body;
-  // Create user name
-  const user_id = users_info.userId;
-  const userName = users_info.userName;
-  const userPassword = users_info.userPass;
-  const userEmail = users_info.userEmail;
-  console.log("user id: " + JSON.stringify(users_info));
- 
-  const querryString = "UPDATE login_info SET userName= ?, userPass=?, userEmail=? where userId=?";
-  mysqlConnection.query(querryString, [userName, userPassword, userEmail, user_id], (err, results, fields) =>{
-    if (err) {
-      console.log("Failed to update new user: " + err);
-      res.sendStatus(500);
-      return;
-    } else {
-      res.send("Update Successfully!");
-    }
-    res.end();
-  })
-})
-
-
-
-
-// router.put('/users',(req,res)=>{
-//     let users_info = req.body;
-//     const user_id = users_info.userId;
-//     const userName = users_info.userName;
-//     const userPassword = users_info.userPass;
-//     const userEmail = users_info.userEmail;
-//     console.log("user id: " + JSON.stringify(users_info));
-
-//     const querryString = "UPDATE login_info SET userName  = ?, userPass = ?, userEmail = ? where userId = ? ";
-//     mysqlConnection.query(querryString, [userName, userPassword, userEmail, user_id], (err, results ,fields) =>{
-//         if(err){
-//             console.log("Update not success: " + err);
-//             res.sendStatus(500);
-//             return;
-//         }
-//         else
-//             console.log("Update success: ");
-        
+// router.delete('/delete_user/:user_id', (req,res) => {
+//     const userid = req.params.user_id;
+//     console.log("ID: " + JSON.stringify(req.body));
+//     mysqlConnection.query('DELETE FROM login_info WHERE userId = ?', [userid], (err, results, fields) =>{
+//         if(!err){
+//             console.log("Successfully Deleted the user: ", results.affectedRows);
+//             res.send("Info has been Deleted");
+//         }else{
+//             console.log(results.insertid + err);
+//         };
+//         res.end();
 //     })
-//     res.end();
+// });
 
-// })
+router.delete('/user_delete', (req,res) => {
+    const userName = req.body.user_name;
+    const userPassword = req.body.user_password;
+    const userEmail = req.body.user_email;
+
+    console.log("user id: " + JSON.stringify(userName));
+    mysqlConnection.query('DELETE FROM login_info WHERE userName = ? AND userPass = ? AND userEmail = ?', [userName, userPassword, userEmail], (err, results, fields) =>{
+        if(!err){
+            console.log("Successfully Deleted the user: ", results.affectedRows);
+            res.send("Info has been Deleted");
+        }else{
+            console.log(err);
+        };
+        res.end();
+    })
+});
+
+
+router.delete('/delete_user', (req,res) => {
+    let users_info = req.body;
+//     const userid = req.body.userId;
+    const userName = req.body.user_name;
+    const userPassword = req.body.user_password;
+    // const userEmail = req.body.user_email;
+    console.log("user id: " + JSON.stringify(users_info));
+    mysqlConnection.query('DELETE FROM login_info WHERE userName = ? AND userPass = ?', [userName, userPassword], (err, results, fields) =>{
+        if(!err){
+            console.log("Successfully Deleted the user: ", results.affectedRows);
+            res.send("Info has been Deleted");
+        }else{
+            console.log(err);
+        };
+        res.end();
+    })
+});
+
+router.delete('/user_delete', (req,res) => {
+    let users_info = req.body;
+    const userid = req.body.userId;
+    console.log("user id: " + JSON.stringify(users_info));
+    console.log("user id: " + JSON.stringify(userid));
+    mysqlConnection.query('DELETE FROM login_info WHERE userId = ?', [userid], (err, results, fields) =>{
+        if(!err){
+            console.log("Successfully Deleted the user: ", results);
+            res.send("Info has been Deleted");
+        }else{
+            console.log(err);
+        };
+        res.end();
+    })
+});
+
+
+router.put('/users',(req,res)=>{
+    let users_info = req.body;
+    const user_id = users_info.userId;
+    const userName = users_info.userName;
+    const userPassword = users_info.userPass;
+    const userEmail = users_info.userEmail;
+    console.log("user id: " + JSON.stringify(users_info));
+
+    const querryString = "UPDATE login_info SET userName  = ?, userPass = ?, userEmail = ? where userId = ? ";
+    mysqlConnection.query(querryString, [userName, userPassword, userEmail, user_id], (err, results ,fields) =>{
+        if(err){
+            console.log("Update not success: " + err);
+            res.sendStatus(500);
+            return;
+        }
+        else
+            console.log("Update success: ");
+        
+    })
+    res.end();
+
+})
 
 
 module.exports = router;
